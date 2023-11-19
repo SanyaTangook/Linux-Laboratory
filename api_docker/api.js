a64f86dd9cb20057e9f8c38e4dacc13deefd5125
SanyaCloudNC <sanya.t@cloudnatician.com>
2023-11-20 03:02:03 +0700

    add new api docker

--- api_docker/api.js
+++ api_docker/api.js
@@ -1,28 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const Docker = require('dockerode');
-const stream = require('stream');
+const Docker = require("dockerode");
+const stream = require("stream");
 
-const docker = new Docker()
+const docker = new Docker();
 
-
-app.post('/containers', (req, res, next) => {
-    docker.createContainer({
-        Image: 'ubuntu',
-        AttachStdin: false,
-        AttachStdout: true,
-        AttachStderr: true,
-        Tty: true,
-    }).then(function (container) {
-        return container.start();
-    }).then((container) => {
-        res.json({message: " start containers complete"});
+app.post("/containers", (req, res) => {
+  docker
+    .createContainer({
+      Image: "ubuntu",
+      AttachStdin: false,
+      AttachStdout: true,
+      AttachStderr: true,
+      Tty: true,
+    })
+    .then(function (container) {
+      return container.start();
+    })
+    .then((container) => {
+      res.json("start containers complete");
     });
-    
 });
 
+app.get("/containerslist", (req, res) => {
+    docker.listContainers(function (err, containers) {
+      const containerObjects = containers.map(element => ({ id: element.Id  , Name: element.Names}));
+      res.json(containerObjects);
+    });
+  });
+  
+  
 
+app.get("/stop/:id", (req, res, next) => {
+  const container = docker.getContainer(req.params.id);
+  container.stop(null, (err, data) => {
+    res.json({ message: "stop containers complete" });
+    console.log(req);
+  });
+});
 
 app.listen(9000, () => {
-    console.log("Starting node.js at port " + 9000);
-  });
+  console.log("Starting node.js at port " + 9000);
+});
