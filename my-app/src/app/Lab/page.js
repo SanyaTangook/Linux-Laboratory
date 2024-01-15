0c345797ab69953d9583738680bd36cf300694b3
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-16 00:05:56 +0700

    edit paeg lab

--- my-app/src/app/Lab/page.js
+++ my-app/src/app/Lab/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
-
-export default function Lab(){
-    return(
-        <>
-            <h1>This my Lab </h1>
-        </>
-    )
+export default function Lab() {
+  return (
+    <>
+      <div>
+        <h1> Lab 1 </h1> <br />
+        <h3> เนื้อหา </h3>
+      </div>
+      <Link href="Lab/console" className="btn btn-success"> เริ่มทดลอง </Link>
+    </>
+  );
 }
