bd1879da7ad427ae80ad400aaf5d286fcade796e
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-01-29 23:34:15 +0700

    Add Api Socker

--- api_docker/api.js
+++ api_docker/api.js
@@ -1,10 +1,124 @@
 import express from "express";
-const app = express();
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 import Docker from "dockerode";
+import  stream  from "stream";
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
 
 const docker = new Docker();
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
+io.on('connection', (socket) => {
+  socket.on('exec', (id, w, h) => {
+      const container = docker.getContainer(id);
+      let cmd = {
+          'AttachStdout': true,
+          'AttachStderr': true,
+          'AttachStdin': true,
+          'Tty': true,
+          Cmd: ['/bin/bash'],
+      };
+      socket.on('resize', (data) => {
+          container.resize({h: data.rows, w: data.cols}, () => {
+          });
+      });
+      container.exec(cmd, (err, exec) => {
+          let options = {
+              'Tty': true,
+              stream: true,
+              stdin: true,
+              stdout: true,
+              stderr: true,
+              // fix vim
+              hijack: true,
+          };
+
+          container.wait((err, data) => {
+              socket.emit('end', 'ended');
+          });
+
+          if (err) {
+              return;
+          }
+
+          exec.start(options, (err, stream) => {
+              stream.on('data', (chunk) => {
+                  socket.emit('show', chunk.toString());
+              });
+
+              socket.on('cmd', (data) => {
+                  if (typeof data !== 'object')
+                      stream.write(data);
+              });
+          });
+      });
+  });
+
+  socket.on('attach', (id, w, h) => {
+      const container = docker.getContainer(id);
+
+      const logStream = new stream.PassThrough();
+      logStream.on('data', (chunk) => {
+          socket.emit('show', chunk.toString('utf8'));
+      });
+
+      const logs_opts = {
+          follow: true,
+          stdout: true,
+          stderr: true,
+          timestamps: false,
+      };
+
+      const handler = (err, stream) => {
+          container.modem.demuxStream(stream, logStream, logStream);
+          if (!err && stream) {
+              stream.on('end', () => {
+                  logStream.end('===Logs stream finished===');
+                  socket.emit('end', 'ended');
+                  stream.destroy();
+              });
+          }
+      };
+
+      container.logs(logs_opts, handler);
+  });
+
+  socket.on('getSysInfo', (id) => {
+      const container = docker.getContainer(id);
+      container.stats((err, stream) => {
+          if (!err && stream != null) {
+              stream.on('data', (data) => {
+                  socket.emit(id, data.toString('utf8'));
+              });
+              stream.on('end', () => {
+                  socket.emit('end', 'ended');
+                  stream.destroy();
+              });
+          }
+      });
+  });
+
+  socket.on('end', () => {
+      array = [];
+      streams.map((stream) => {
+          stream.destroy();
+      });
+      console.log('--------end---------');
+  });
+
+  let array = [];
+  let streams = [];
+});
 app.post("/containers/:Name", (req, res) => {
   docker
     .createContainer({
@@ -16,7 +130,8 @@ app.post("/containers/:Name", (req, res) => {
     })
     .then((container) => container.start())
     .then(() => {
-        res.json(`start containers complete`);
+      res.status(200);
+      res.json(`start containers ${req.params.Name} complete`);
     });
 });
 
@@ -37,16 +152,13 @@ app.get("/stop/:id", (req, res, next) => {
   });
 });
 
-app.get('/remove/:id', (req, res, next) => {
+app.get("/remove/:id", (req, res, next) => {
   const container = docker.getContainer(req.params.id);
   container.remove({ force: true }, (err, data) => {
-    res.json("remove containers complete ")
+    res.json("remove containers complete ");
   });
 });
 
-
-
-app.listen(9000, () => {
-  console.log("Starting node.js at port " + 9000);
+server.listen(3001, () => {
+  console.log("Starting node.js at port " + 3001);
 });
-
