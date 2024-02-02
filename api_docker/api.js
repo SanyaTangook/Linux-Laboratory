7d34897a2d79fca14838ee6c3a52991b5ce4bc76
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-02-02 16:15:03 +0700

    Api and Page Term

--- api_docker/api.js
+++ api_docker/api.js
@@ -3,28 +3,131 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import Docker from "dockerode";
-import  stream  from "stream";
+import stream from "stream";
+
 
 const app = express();
 app.use(cors());
+app.set ( "view engine", "ejs" );
+
 const server = http.createServer(app);
 
 const docker = new Docker();
 
-const io = new Server(server, {
+const io = new Server((server)
+  ,{
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
 
 io.on('connection', (socket) => {
-    console.log(`ID: ${socket.id}`)
-    socket.on('exec' ,(msg) =>{
-      console.log(msg)
-    socket.emit('data' , msg);
-    })
+  socket.on('exec', (id, w, h) => {
+      const container = docker.getContainer(id);
+      let cmd = {
+          'AttachStdout': true,
+          'AttachStderr': true,
+          'AttachStdin': true,
+          'Tty': true,
+          Cmd: ['/bin/bash'],
+      };
+      socket.on('resize', (data) => {
+          container.resize({h: data.rows, w: data.cols}, () => {
+          });
+      });
+      container.exec(cmd, (err, exec) => {
+          let options = {
+              'Tty': true,
+              stream: true,
+              stdin: true,
+              stdout: true,
+              stderr: true,
+              // fix vim
+              hijack: true,
+          };
+
+          container.wait((err, data) => {
+              socket.emit('end', 'ended');
+          });
+
+          if (err) {
+              return;
+          }
+
+          exec.start(options, (err, stream) => {
+              stream.on('data', (chunk) => {
+                  socket.emit('show', chunk.toString());
+              });
+
+              socket.on('cmd', (data) => {
+                  if (typeof data !== 'object')
+                      stream.write(data);
+              });
+          });
+      });
   });
+
+  socket.on('attach', (id, w, h) => {
+      const container = docker.getContainer(id);
+
+      const logStream = new stream.PassThrough();
+      logStream.on('data', (chunk) => {
+          socket.emit('show', chunk.toString('utf8'));
+      });
+
+      const logs_opts = {
+          follow: true,
+          stdout: true,
+          stderr: true,
+          timestamps: false,
+      };
+
+      const handler = (err, stream) => {
+          container.modem.demuxStream(stream, logStream, logStream);
+          if (!err && stream) {
+              stream.on('end', () => {
+                  logStream.end('===Logs stream finished===');
+                  socket.emit('end', 'ended');
+                  stream.destroy();
+              });
+          }
+      };
+
+      container.logs(logs_opts, handler);
+  });
+
+  socket.on('getSysInfo', (id) => {
+      const container = docker.getContainer(id);
+      container.stats((err, stream) => {
+          if (!err && stream != null) {
+              stream.on('data', (data) => {
+                  socket.emit(id, data.toString('utf8'));
+              });
+              stream.on('end', () => {
+                  socket.emit('end', 'ended');
+                  stream.destroy();
+              });
+          }
+      });
+  });
+
+  socket.on('end', () => {
+      array = [];
+      streams.map((stream) => {
+          stream.destroy();
+      });
+      console.log('--------end---------');
+  });
+
+  let array = [];
+  let streams = [];
+});
+
+
+app.get('/console', (req, res, next) => {
+  res.render('terminal')
+});
 
 app.post("/containers/:Name", (req, res) => {
   docker
@@ -41,7 +144,6 @@ app.post("/containers/:Name", (req, res) => {
       res.json(`start containers ${req.params.Name} complete`);
     });
 });
-
 app.get("/containerslist", (req, res) => {
   docker.listContainers((err, containers) => {
     const containerObjects = containers.map((element) => ({
@@ -51,6 +153,8 @@ app.get("/containerslist", (req, res) => {
     res.json(containerObjects);
   });
 });
+
+
 
 app.get("/stop/:id", (req, res, next) => {
   const container = docker.getContainer(req.params.id);
--- my-app/src/app/Lab/page.js
+++ my-app/src/app/Lab/page.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
 export default function Lab() {
+
   return (
     <>
       <div>
         <h1> Lab 1 </h1> <br />
         <h3> เนื้อหา </h3>
       </div>
-      <Link href="Lab/console" className="btn btn-success" target="_blank"> เริ่มทดลอง </Link>
+      <Link href="http://localhost:3001/console" className="btn btn-success" target="_blank" > เริ่มทดลอง </Link>
     </>
   );
 }
